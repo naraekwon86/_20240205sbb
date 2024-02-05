9d640adeb618a7816f3aded4a864f8b0cdc7be92
naraekwon86 <wde0530@gmail.com>
2024-02-05 13:41:05 +0900

    start

---
+++ src/test/java/com/ll/_20240205sbb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll._20240205sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
